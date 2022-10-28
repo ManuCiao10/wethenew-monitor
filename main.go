9851a80a0bd299bfaff467e77b2493b2651c63fa
ManuCiao10 <emanuele.ardinghi@gmail.com>
2022-10-27 23:35:53 -0400

    req

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+
 	// "io/ioutil"
 	"log"
 	"time"
@@ -14,6 +15,7 @@ import (
 	http "github.com/bogdanfinn/fhttp"
 	tls_client "github.com/bogdanfinn/tls-client"
 	"github.com/joho/godotenv"
+	"golang.org/x/vuln/client"
 )
 
 type Login struct {
@@ -45,6 +47,7 @@ func timer(name string) func() {
 	}
 }
 
+
 func Login_init(client tls_client.HttpClient) {
 	url := "https://sell.wethenew.com/sell-now"
 	req, err := http.NewRequest("GET", url, nil)
@@ -55,7 +58,7 @@ func Login_init(client tls_client.HttpClient) {
 	req.Header.Set("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
 	req.Header.Set("accept-language", "it-IT,it;q=0.9,en-US;q=0.8,en;q=0.7,de;q=0.6,fr;q=0.5")
 	req.Header.Set("cache-control", "no-cache")
-	req.Header.Set("cookie", "_ga=GA1.1.2092679579.1666843583; euconsent-v2=CPhgeQAPhgeQAAHABBENCmCgAP_AAAEAAAqIAAAAAAAA.f_gAACAAAAAA; _cs_c=1; didomi_token=eyJ1c2VyX2lkIjoiMTg0MTcxZTItZTI3OC02MzM5LTgxYzgtNzVlOWNiNzUwYTk4IiwiY3JlYXRlZCI6IjIwMjItMTAtMjdUMDQ6MDY6MjkuNzUzWiIsInVwZGF0ZWQiOiIyMDIyLTEwLTI3VDA0OjA2OjI5Ljc1M1oiLCJ2ZXJzaW9uIjoyLCJ2ZW5kb3JzIjp7ImVuYWJsZWQiOlsiYzpjb250ZW50c3F1YXJlIiwiYzpnb29nbGVhbmEtUkVVeXFXRHciXX0sInZlbmRvcnNfbGkiOnsiZW5hYmxlZCI6WyJjOmdvb2dsZWFuYS1SRVV5cVdEdyJdfX0=; _gid=GA1.2.445192787.1666843838; _cs_id=eda9db4c-4504-ac8b-f794-d826b8156749.1666843588.1.1666843838.1666843588.1.1701007588298; slrspc_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImVtYW51ZWxlLmFyZGluZ2hpQGdtYWlsLmNvbSIsImZpcnN0bmFtZSI6ImVtYW51ZWxlIiwibGFzdG5hbWUiOiJhcmRpbmdoaSIsImlhdCI6MTY2Njg0Mzg1NywiZXhwIjoxNjcyMDI3ODU3fQ.0ZqIsxYCTiu8n44Nw9M9OFFdF1huH1shBItHsFUlU1g; slrspc_refresh_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImVtYW51ZWxlLmFyZGluZ2hpQGdtYWlsLmNvbSIsImZpcnN0bmFtZSI6ImVtYW51ZWxlIiwibGFzdG5hbWUiOiJhcmRpbmdoaSIsImlhdCI6MTY2Njg0Mzg1NywiZXhwIjoxNjY3NDQ4NjU3fQ.b8a4lLqMRdtTA9QTwT1dk8IMZQK2rPy7GC56_GWYWCQ; ABTasty=uid=j9y47r4wpkgk69zn&fst=1666843582502&pst=-1&cst=1666843582502&ns=1&pvt=4&pvis=4&th=; _ga_FTYGFSRM68=GS1.1.1666843583.1.1.1666844496.0.0.0")
+	req.Header.Set("cookie", "euconsent-v2=CPhgeQAPhgeQAAHABBENCmCgAP_AAAEAAAqIAAAAAAAA.f_gAACAAAAAA; _cs_c=1; didomi_token=eyJ1c2VyX2lkIjoiMTg0MTcxZTItZTI3OC02MzM5LTgxYzgtNzVlOWNiNzUwYTk4IiwiY3JlYXRlZCI6IjIwMjItMTAtMjdUMDQ6MDY6MjkuNzUzWiIsInVwZGF0ZWQiOiIyMDIyLTEwLTI3VDA0OjA2OjI5Ljc1M1oiLCJ2ZXJzaW9uIjoyLCJ2ZW5kb3JzIjp7ImVuYWJsZWQiOlsiYzpjb250ZW50c3F1YXJlIiwiYzpnb29nbGVhbmEtUkVVeXFXRHciXX0sInZlbmRvcnNfbGkiOnsiZW5hYmxlZCI6WyJjOmdvb2dsZWFuYS1SRVV5cVdEdyJdfX0=; _gid=GA1.2.445192787.1666843838; _cs_id=eda9db4c-4504-ac8b-f794-d826b8156749.1666843588.1.1666843838.1666843588.1.1701007588298; slrspc_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImVtYW51ZWxlLmFyZGluZ2hpQGdtYWlsLmNvbSIsImZpcnN0bmFtZSI6ImVtYW51ZWxlIiwibGFzdG5hbWUiOiJhcmRpbmdoaSIsImlhdCI6MTY2Njg0Mzg1NywiZXhwIjoxNjcyMDI3ODU3fQ.0ZqIsxYCTiu8n44Nw9M9OFFdF1huH1shBItHsFUlU1g; slrspc_refresh_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImVtYW51ZWxlLmFyZGluZ2hpQGdtYWlsLmNvbSIsImZpcnN0bmFtZSI6ImVtYW51ZWxlIiwibGFzdG5hbWUiOiJhcmRpbmdoaSIsImlhdCI6MTY2Njg0Mzg1NywiZXhwIjoxNjY3NDQ4NjU3fQ.b8a4lLqMRdtTA9QTwT1dk8IMZQK2rPy7GC56_GWYWCQ; ABTasty=uid=j9y47r4wpkgk69zn&fst=1666843582502&pst=-1&cst=1666843582502&ns=1&pvt=4&pvis=4&th=; _ga_FTYGFSRM68=GS1.1.1666843583.1.1.1666844496.0.0.0")
 	req.Header.Set("pragma", "no-cache")
 	req.Header.Set("sec-ch-ua", `"Chromium";v="106", "Google Chrome";v="106", "Not;A=Brand";v="99"`)
 	req.Header.Set("sec-ch-ua-mobile", "?0")
@@ -77,6 +80,7 @@ func Login_init(client tls_client.HttpClient) {
 	}
 	fmt.Println(string(body))
 	fmt.Println(response.StatusCode)
+	
 
 }
 
@@ -95,6 +99,8 @@ func main() {
 		log.Fatal(err)
 	}
 	Login_init(client)
+	
+
 	// app := fiber.New()
 	// login := Login{
 	// 	Token:    os.Getenv("TOKEN"),
@@ -108,3 +114,6 @@ func main() {
 //parse the html 
 //save data and create a for loop 
 //check only new data at index 0
+//check cache to do not do more requests
+//check if cookies expired or try to do the login
+
