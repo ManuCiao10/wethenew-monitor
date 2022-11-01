078027afbdd9271f10011d7b8c47a11930dd3496
ManuCiao10 <emanuele.ardinghi@gmail.com>
2022-10-31 23:55:16 -0400

    for loop monitoring

--- Monitor/monitor.go
+++ Monitor/monitor.go
@@ -1,31 +1,12 @@
 package monitor
 
 import (
-	// "bytes"
-	// "bytes"
 	"encoding/json"
-
-	// "fmt"
-	// "strconv"
-
-	// "net/http/cookiejar"
-	// "os"
-
-	// "os"
-	// "strconv"
-	// "time"
-
-	// "fmt"
+	"fmt"
 	"io"
 	"log"
 	"main/data"
 	"main/discord"
-
-	// "os"
-	// "strconv"
-	// "time"
-
-	// "main/discord"
 
 	http "github.com/bogdanfinn/fhttp"
 	tls_client "github.com/bogdanfinn/tls-client"
@@ -61,38 +42,45 @@ func Contains(s []int, id int) bool {
 }
 
 func MonitorProducts(class data.Info, client tls_client.HttpClient) {
-	url := "https://api-sell.wethenew.com/sell-nows?skip=0&take=50"
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	req.Header = http.Header{
-		"Accept":          {"application/json, text/plain, */*"},
-		"accept-language": {"it-IT,it;q=0.9,en-US;q=0.8,en;q=0.7,de;q=0.6,fr;q=0.5"},
-		"user-agent":      {"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"},
-		http.HeaderOrderKey: {
-			"Accept",
-			"accept-language",
-			"user-agent",
-		},
-	}
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-	body, _ := io.ReadAll(resp.Body)
-	var new_id data.ID
-	if err := json.Unmarshal(body, &new_id); err != nil {
-		log.Fatal(err)
-	}
 	Slice := SaveSliceTest(class) //TRY TO USE THE NEW_ID TO ADD THE FIRST TIME ALL THE PRODUCTS AND AFTER USEA WHILE LOOP OR A TIMER OUT FOR REQUEST
-
-	for idx, v := range new_id.Results {
-		if !Contains(Slice, v.ID) {
-			Slice = append(Slice, v.ID)
-			discord.Webhook(new_id, idx)
+	url := "https://api-sell.wethenew.com/sell-nows?skip=0&take=50"
+	for {
+		req, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			log.Fatal(err)
 		}
-	}
+		req.Header = http.Header{
+			"Accept":          {"application/json, text/plain, */*"},
+			"accept-language": {"it-IT,it;q=0.9,en-US;q=0.8,en;q=0.7,de;q=0.6,fr;q=0.5"},
+			"user-agent":      {"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"},
+			http.HeaderOrderKey: {
+				"Accept",
+				"accept-language",
+				"user-agent",
+			},
+		}
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer resp.Body.Close()
+		body, _ := io.ReadAll(resp.Body)
+		var new_id data.ID
+		if err := json.Unmarshal(body, &new_id); err != nil {
+			log.Fatal(err)
+		}
+		
+		for idx, v := range new_id.Results {
+			if !Contains(Slice, v.ID) {
+				Slice = append(Slice, v.ID)
+				discord.Webhook(new_id, idx)
+				continue
+			} else {
+				fmt.Print("MONITORING", resp.StatusCode)
+			}
+		}
+
+	};
+
 
 }
