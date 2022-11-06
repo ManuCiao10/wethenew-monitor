a099f1162dd753e71da0379ba2ccb31aee3a5066
ManuCiao10 <emanuele.ardinghi@gmail.com>
2022-11-06 17:24:52 -0500

    imporvement speed

--- Monitor/monitor.go
+++ Monitor/monitor.go
@@ -30,41 +30,39 @@ func MonitorProducts(class data.Info) {
 	// c.Delete(fmt.Sprintf("%d",1691)) For testing
 	fmt.Print("Cache: ", c.Items())
 	url := "https://api-sell.wethenew.com/sell-nows?skip=0&take=50"
+	options := []tls_client.HttpClientOption{
+		tls_client.WithTimeout(30),
+		tls_client.WithClientProfile(tls_client.Chrome_105),
+		tls_client.WithNotFollowRedirects(),
+		tls_client.WithProxyUrl(discord.GetProxy()),
+	}
+
+	client, err := tls_client.NewHttpClient(tls_client.NewNoopLogger(), options...)
+	if err != nil {
+		log.Print(err)
+	}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	req.Header = http.Header{
+		"Accept":          {"application/json, text/plain, */*"},
+		"accept-language": {"it-IT,it;q=0.9,en-US;q=0.8,en;q=0.7,de;q=0.6,fr;q=0.5"},
+		"user-agent":      {"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"},
+		http.HeaderOrderKey: {
+			"Accept",
+			"accept-language",
+			"user-agent",
+		},
+	}
 	for {
-		options := []tls_client.HttpClientOption{
-			tls_client.WithTimeout(30),
-			tls_client.WithClientProfile(tls_client.Chrome_105),
-			tls_client.WithNotFollowRedirects(),
-			tls_client.WithProxyUrl(discord.GetProxy()),
-		}
-
-		client, err := tls_client.NewHttpClient(tls_client.NewNoopLogger(), options...)
-		if err != nil {
-			log.Print(err)
-		}
-
-		req, err := http.NewRequest("GET", url, nil)
-		if err != nil {
-			log.Println(err)
-			continue
-		}
-		req.Header = http.Header{
-			"Accept":          {"application/json, text/plain, */*"},
-			"accept-language": {"it-IT,it;q=0.9,en-US;q=0.8,en;q=0.7,de;q=0.6,fr;q=0.5"},
-			"user-agent":      {"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"},
-			http.HeaderOrderKey: {
-				"Accept",
-				"accept-language",
-				"user-agent",
-			},
-		}
 		// time.Sleep(time.Duration(3) * time.Second)
 		resp, err := client.Do(req)
 		if err != nil {
 			log.Println(err)
 			continue
 		}
-		fmt.Printf("[%s] Status code: <|%d|> \n", Time(), resp.StatusCode)
+		fmt.Printf("[+] %s Status code: <|%d|> \n", Time(), resp.StatusCode)
 		body, _ := io.ReadAll(resp.Body)
 		_ = resp.Body.Close()
 		var new_id data.ID
